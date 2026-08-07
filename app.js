47bbc5663b8973e2c083a50cef185af39d92b2e7
Shivam Kumar Roy <8298689524sroy@gmail.com>
2026-08-07 09:37:58 +0530

    install commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// const fs = require('fs');
+// const data = fs.readFileSync('notes.txt', 'utf-8');
+// console.log(data);
+// fs.writeFileSync('notes.txt', 'This is Priyanshu babu');
+
+// const path = require('path');
+// const filepath = path.join(__dirname,'test', 'hello.txt');
+// console.log(filepath);
+
+// const os = require('os');
+// const user = os.userInfo();
+// console.log(user);
+
+// const des = require('./data/des');
+// console.log(des);
+
+// const package = require('./data/package');
+// console.log(package);   
