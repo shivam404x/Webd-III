fee9021f8ce6eac235b72b7fe091660c9bfc8aca
Shivam Kumar Roy <8298689524sroy@gmail.com>
2026-09-08 14:03:49 +0530

    Refactor index.js to use tourRoutes and clean up code

--- UNIT_2/index.js
+++ UNIT_2/index.js
@@ -1,42 +1,37 @@
 const express = require("express");
 const app = express();
-
-const packages = require("./data/tour.js");
-
-// const user = [{
-//   "name": "Shivam",
-//   "age": 21,
-// },
-// {
-//   "name": "John",
-//   "age": 30,  
-// }]
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello, Babu!");
-});
+const tourRoutes = require("./route/tourRoutes");
 
-app.get("/packages", (req, res) => {
-  const destination = req.query.destination;
-  if (!destination) {
-    res.json(packages);
-  }
-  const filteredPackages = packages.filter((pkg) => pkg.destination === destination);
-  res.json(filteredPackages);
-});
+app.use("/api", tourRoutes);
 
-app.get("/packages/:id", (req, res) => {
-  const packageId = parseInt(req.params.id);
-  const tourPackage = packages.find((pkg) => pkg.id === packageId);
-  res.send(tourPackage);
-});
 
-app.post('/packages', (req, res) => {
-  const newPackage = req.body;
-  packages.push(newPackage);
-  res.status(201).json(newPackage);
-});
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("Hello, Babu!");
+// });
+
+// app.get("/packages", (req, res) => {
+//   const destination = req.query.destination;
+//   if (!destination) {
+//     res.status(200).json(packages);
+//   }
+//   const filteredPackages = packages.filter((pkg) => pkg.destination.toLocaleLowerCase() === destination.toLocaleLowerCase());
+//   res.status(200).json(filteredPackages);
+// });
+
+// app.get("/packages/:id", (req, res) => {
+//   const packageId = parseInt(req.params.id);
+//   const tourPackage = packages.find((pkg) => pkg.id === packageId);
+//   res.status(200).json(tourPackage);
+// });
+
+// app.post('/packages', (req, res) => {
+//   const newPackage = req.body;
+//   packages.push(newPackage);
+//   res.status(201).json(newPackage);
+// });
 
 // app.put('/user/:id',(req,res) => {
 //   console.log(req.params.id);
@@ -46,4 +41,4 @@ app.post('/packages', (req, res) => {
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-});
+});  
